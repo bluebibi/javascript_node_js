475b7e0b5d24bc6c91102172a2eb5a6280b4ae3a
Youn-Hee Han <yh21.han@gmail.com>
2014-09-09 11:38:13 +0900

    no message

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+//number
+var intNum = 10;
+var floatNum = 0.1;
+
+//string
+var singleQuoteStr = 'single quote string';
+var doubleQuoteStr = "single quote string";
+
+//boolean
+var boolVar = true;
+
+//undefined
+var undefinedVar;
+
+//null
+var nullVar = null;
+
+console.log(typeof intNum, typeof floatNum, typeof singleQuoteStr, typeof doubleQuoteStr, typeof boolVar, typeof undefinedVar, typeof nullVar);
