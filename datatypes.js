2405261171a0c9fd1db1f1d791a011c723c1f948
Youn-Hee Han <yh21.han@gmail.com>
2014-09-09 12:53:22 +0900

    no message

--- datatypes.js
+++ datatypes.js
@@ -1,18 +1,39 @@
 //number
 var intNum = 10;
 var floatNum = 0.1;
+console.log(typeof intNum, typeof floatNum)
+var num = 5 / 2;
+console.log(num);
+console.log(typeof num);
+console.log(Math.floor(num));
+console.log()
 
 //string
 var singleQuoteStr = 'single quote string';
 var doubleQuoteStr = "single quote string";
+console.log(typeof singleQuoteStr, typeof doubleQuoteStr)
+var str = "Javascript";
+console.log(str);
+console.log(typeof str);
+console.log(str.length);
+console.log(str[0], str[1], str[2], str[9], str[10]);
+str[0] = 'T';
+console.log(str);
+console.log(str[0]);
+console.log()
 
 //boolean
 var boolVar = true;
+var boolVar2 = false;
+console.log(typeof boolVar, typeof boolVar2);
+console.log()
 
 //undefined
 var undefinedVar;
+console.log(typeof undefinedVar);
+console.log()
 
 //null
 var nullVar = null;
-
-console.log(typeof intNum, typeof floatNum, typeof singleQuoteStr, typeof doubleQuoteStr, typeof boolVar, typeof undefinedVar, typeof nullVar); 
+console.log(typeof nullVar); 
+console.log()
